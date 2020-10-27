476ce6b57b8fa05dd9c2525940a4eae7c07fe2de
Cristina <cristinac.vaillant@gmail.com>
2020-10-27 01:19:37 +0100

    Adicionando clases al proyecto

---
+++ CatalogoMicroservice/Catalogo/Models/Combo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalogo.Models
+{
+    public class Combo
+    {
+        public Guid ComboId { get; set; }
+        public Guid EventId { get; set; }
+        public string  Category { get; set; }
+        public string Name { get; set; }
+        public int Precio { get; set; }
+
+    }
+}
