153ea67cb6af06eb9a0017bf970f64262952d802
Cristina <cristinac.vaillant@gmail.com>
2020-10-28 22:48:24 +0100

    Revisar modelo de clases y sus atributos close #24

--- CatalogoMicroservice/Catalogo/Models/Combo.cs
+++ CatalogoMicroservice/Catalogo/Models/Combo.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace Catalogo.Models
 {
     public class Combo
     {
-        public Guid ComboId { get; set; }
-        public Guid EventId { get; set; }
-        public string  Category { get; set; }
-        public string Name { get; set; }
-        public int Precio { get; set; }
-
+        private Guid ComboId { get; set; }
+        private Guid EventId { get; set; }
+        private enum  Category {niño, mujer, hombre}
+        private string Name { get; set; }
+        private int Precio { get; set; }
     }
 }
