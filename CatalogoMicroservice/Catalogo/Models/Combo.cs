d011a55ac85b6a80a7041d52ed6a69944511fff6
Cristina <cristinac.vaillant@gmail.com>
2020-10-29 00:38:32 +0100

    Revisar modelo de clases y sus atributos close #24

--- CatalogoMicroservice/Catalogo/Models/Combo.cs
+++ CatalogoMicroservice/Catalogo/Models/Combo.cs
@@ -5,10 +5,39 @@ namespace Catalogo.Models
 {
     public class Combo
     {
-        private Guid ComboId { get; set; }
-        private Guid EventId { get; set; }
-        private enum  Category {niño, mujer, hombre}
-        private string Name { get; set; }
-        private int Precio { get; set; }
+        private Guid _ComboId;
+        private Guid _EventId;
+        private string _Name; 
+        private int _Precio;
+        
+        public Guid ComboId
+     {
+         get { return ComboId; }
+         set { ComboId = value; }
+     }
+        public Guid EventId
+     {
+         get { return EventId; }
+         set { EventId = value; }
+     }
+     
+       public string Name
+     {
+         get { return Name; }
+         set { Name = value; }
+     }
+       public int Precio
+     {
+         get { return Precio; }
+         set { Precio = value; }
+     }
+
     }
+    public enum Category 
+        {
+            niño = 0, 
+            mujer = 1, 
+            hombre =2
+        };
+        
 }
--- CatalogoMicroservice/Catalogo/Models/Evento.cs
+++ CatalogoMicroservice/Catalogo/Models/Evento.cs
@@ -6,11 +6,45 @@ namespace Catalogo.Models
 {
     public class Evento
     {
-        private Guid EventId { get; set; }
-        private string Name { get; set; }
-        private DateTime Date { get; set; }
-        private string Description { get; set; }
-        private string Pais { get; set; }
-        private List<Combo> listcombos { get; set; }
+        private Guid _EventId;
+        private string _Name;
+        private DateTime _Date;
+        private string _Description;
+        private string _Pais;
+        private List<Combo> _listcombos;
+
+         public Guid EventId
+     {
+         get { return EventId; }
+         set { EventId= value; }
+     }
+        public string Name
+     {
+         get { return Name; }
+         set { Name = value; }
+     }
+    
+        public DateTime Date
+     {
+         get { return Date; }
+         set { Date = value; }
+     }
+     
+       public string Description
+     {
+         get { return Description; }
+         set { Description = value; }
+     }
+       public string Pais
+     {
+         get { return Pais; }
+         set { Pais = value; }
+     }
+       public List<Combo> listcombos
+     {
+         get { return listcombos; }
+         set { listcombos = value; }
+     }
+
     }
 }
