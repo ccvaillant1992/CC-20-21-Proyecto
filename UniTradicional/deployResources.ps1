4c94c44ef5a27820ed22d79669feff944a58efe8
Cristina <cristinac.vaillant@gmail.com>
2020-11-10 21:19:34 +0100

    Añade Fichero de Tarea para desplegar Infraestructura close #31

---
+++ UniTradicional/deployResources.ps1
@@ -0,0 +1,25 @@
+param (
+    [Parameter(Mandatory = $true)] [string] $ResourceGroupName,
+    [Parameter(Mandatory = $false)] [string] $resourcePrefix,
+    [Parameter(Mandatory = $false)] [string] $sqlAdminUsername,
+    [Parameter(Mandatory = $false)] [string] $sqlAdminPassword
+)
+
+#Gets the ARM template file
+Write-Host "Getting ARM template file..."
+$TemplateLocation = Join-Path  $PSScriptRoot "resources.json"
+if($null -eq $TemplateLocation) {
+    throw "No ARM template file was found in path $TemplateFile"
+} else {
+    Write-Host "OK" -ForegroundCoor Green
+}
+
+# Set of parameters for ARM template. 
+$Parameters = @{ } #hashtable = collection of name-value pairs
+$Parameters.Add("resourcePrefix", $resourcePrefix)
+$Parameters.Add("sqlAdminUsername", $sqlAdminUsername)
+$Parameters.Add("sqlAdminPassword", $sqlAdminPassword)
+
+#$Deployment = New-AzResourceGroupDeployment -Name "nhg-resources-$((Get-Date).ToString('yyyy-MM-dd_HH-mm'))"  -ResourceGroupName $ResourceGroupName  -TemplateFile $TemplateLocation -Mode Incremental -TemplateParameterObject $Parameters -Verbose
+$Deployment = New-AzResourceGroupDeployment  -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateLocation -Mode Incremental -TemplateParameterObject $Parameters -Verbose
+
